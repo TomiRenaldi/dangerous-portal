0b32e1dab3d06239548330a8d60fcd6133e17a8d
TomiRenaldi <tomirenaldi2@gmail.com>
2022-07-03 21:09:20 +0700

    :construction: View > lights > Add

--- src/Experience/Lights.js
+++ src/Experience/Lights.js
@@ -10,11 +10,11 @@ export default class Lights
         this.scene = this.experience.scene
         this.time = this.experience.time
 
-        // Debug
-        this.debugFolder = this.debug.addFolder({
-            title: 'lights',
-            expanded: false,
-        })
+        // // Debug
+        // this.debugFolder = this.debug.addFolder({
+        //     title: 'lights',
+        //     expanded: false,
+        // })
 
         this.setPointLights()
     }
@@ -36,37 +36,37 @@ export default class Lights
         this.pointLights.instance.shadow.mapSize.y = 1024
         this.scene.add(this.pointLights.instance)
 
-        // Debug
-        this.debugFolder
-            .addInput(
-                this.pointLights,
-                'color',
-                { view: 'color' }
-            )
-            .on('change', () => {
-                this.pointLights.instance.color.set(this.pointLights.color)
-            })
+        // // Debug
+        // this.debugFolder
+        //     .addInput(
+        //         this.pointLights,
+        //         'color',
+        //         { view: 'color' }
+        //     )
+        //     .on('change', () => {
+        //         this.pointLights.instance.color.set(this.pointLights.color)
+        //     })
 
-        this.debugFolder
-            .addInput(
-                this.pointLights,
-                'intensity',
-                { min: 0, max: 10 }
-            )
+        // this.debugFolder
+        //     .addInput(
+        //         this.pointLights,
+        //         'intensity',
+        //         { min: 0, max: 10 }
+        //     )
 
-        this.debugFolder
-            .addInput(
-                this.pointLights.instance,
-                'decay',
-                { min: 0, max: 10 }
-            )
+        // this.debugFolder
+        //     .addInput(
+        //         this.pointLights.instance,
+        //         'decay',
+        //         { min: 0, max: 10 }
+        //     )
 
-        this.debugFolder
-            .addInput(
-                this.pointLights.position,
-                'y',
-                { min: 0, max: 10 }
-            )
+        // this.debugFolder
+        //     .addInput(
+        //         this.pointLights.position,
+        //         'y',
+        //         { min: 0, max: 10 }
+        //     )
     }
 
     update()
